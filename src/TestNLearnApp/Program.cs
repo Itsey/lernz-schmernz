80e095854b9e00fa545a133c0b2140664145a882
Jim <jim@plisky.net>
2026-01-04 15:08:48 +0000

    chore: initial app added

--- src/TestNLearnApp/Program.cs
+++ src/TestNLearnApp/Program.cs
@@ -1,7 +1,37 @@
-﻿namespace TestNLearnApp;
+﻿using System.Diagnostics;
+using Plisky.Diagnostics;
+using Plisky.Diagnostics.Listeners;
+
+namespace TestNLearnApp;
 
 internal class Program {
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+
+        Bilge.SetConfigurationResolver((a, b) => {
+            return SourceLevels.Verbose;
+        });
+        var b = new Bilge("testnlearn-main", tl: SourceLevels.Verbose);
+
+        var thnd = new TCPHandler("127.0.0.1", 9060);
+        thnd.SetFormatter(new FlimFlamV4Formatter());
+
+
+        var thnd2 = new InMemoryHandler();
+        thnd2.SetFormatter(new FlimFlamV4Formatter());
+
+        Bilge.AddHandler(thnd);
+        Bilge.AddHandler(thnd2);
+
+        for (int i = 0; i < 10; i++) {
+            Thread.Sleep(100);
+            b.Info.Log("Starting Test - Timings");
+        }
+
+        if (thnd2.GetMessageCount() < 9) {
+            throw new Exception("Did not receive expected messages in in-memory handler");
+        }
+
+
+
     }
 }
---
+++ src/TnLSite/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace TnLSite.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
